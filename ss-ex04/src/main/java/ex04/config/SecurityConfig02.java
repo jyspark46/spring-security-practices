3215a1f830f232ed250495ad52083ca3e27826e7
jelypvid <jyspark46@gmail.com>
2024-06-21 11:06:26 +0900

    spring security ex04 #finish

--- ss-ex04/src/main/java/ex04/config/SecurityConfig02.java
+++ ss-ex04/src/main/java/ex04/config/SecurityConfig02.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig02 {
+	@Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return new WebSecurityCustomizer() {
             @Override
@@ -25,6 +26,18 @@ public class SecurityConfig02 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        http
+        	.formLogin() // configure 생성
+        	.and() // configure 끝 ??
+        	.httpBasic(); // BasicAuthenticationFilter
+        
+        // 위의 and()의 역할 - 연결 (새로운 builder)
+//      http
+//	    	.formLogin()
+//	    	.loginPage("/user/login");
+//	    http
+//	    	.httpBasic();
+	    
+    	return http.build();
     }
 }
